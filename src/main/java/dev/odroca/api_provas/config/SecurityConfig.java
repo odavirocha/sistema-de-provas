109364a1340ac56c31a0ced25853ec96c98049bd
odroca <davirochatz@gmail.com>
2025-10-25 18:09:15 -0300

    Spring Security

--- src/main/java/dev/odroca/api_provas/config/SecurityConfig.java
+++ src/main/java/dev/odroca/api_provas/config/SecurityConfig.java
@@ -1,12 +1,36 @@
 package dev.odroca.api_provas.config;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
     
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+    
+    @Bean
+    private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
     
 }
