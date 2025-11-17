1a0c3042d4675c37039ec0d6e8c9e11d06d328f9
oDroca <davirochatz@gmail.com>
2025-11-17 14:28:04 -0300

    Removendo um aviso.

--- src/main/java/dev/odroca/api_provas/mapper/TestMapper.java
+++ src/main/java/dev/odroca/api_provas/mapper/TestMapper.java
@@ -3,6 +3,7 @@ package dev.odroca.api_provas.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import dev.odroca.api_provas.entity.TestEntity;
 import dev.odroca.api_provas.model.TestModelDTO;
@@ -10,6 +11,7 @@ import dev.odroca.api_provas.model.TestModelDTO;
 @Mapper(componentModel = "spring", uses = QuestionMapper.class)
 public interface TestMapper {
     
+    @Mapping(target = "userId", ignore = true)
     TestEntity toEntity(TestModelDTO testModelDTO);
 
     TestModelDTO toDto(TestEntity testEntity);
