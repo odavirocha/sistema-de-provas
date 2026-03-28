911273db9ff45b851401ad3cd13ed49fb3bb4e26
Davi Rocha <davirochatz@gmail.com>
2026-03-28 15:14:52 -0300

    Teste de sucesso TestNotFoundException

--- src/test/java/dev/odroca/api_provas/service/TestFactory.java
+++ src/test/java/dev/odroca/api_provas/service/TestFactory.java
@@ -1,9 +1,14 @@
 package dev.odroca.api_provas.service;
 
+import dev.odroca.api_provas.dto.question.QuestionAnswerModelDTO;
+import dev.odroca.api_provas.dto.test.AnswerTestRequestDTO;
+import dev.odroca.api_provas.entity.OptionEntity;
 import dev.odroca.api_provas.entity.TestEntity;
 import dev.odroca.api_provas.entity.UserEntity;
+import dev.odroca.api_provas.exception.OptionNotFoundException;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.List;
 import java.util.UUID;
 
 public class TestFactory {
@@ -12,5 +17,18 @@ public class TestFactory {
         ReflectionTestUtils.setField(test, "id", testId);
         test.setQuestions(QuestionFactory.buildQuestionEntity(test));
         return test;
+    }
+
+    public static AnswerTestRequestDTO buildRequestTest(TestEntity godTest) {
+        List<QuestionAnswerModelDTO> answerQuestions = godTest.getQuestions().stream().map(question -> {
+            UUID id = question.getId();
+            UUID selectedOptionId = question.getOptions().stream()
+            .filter(OptionEntity::getIsCorrect)
+            .map(OptionEntity::getId).findFirst().orElseThrow(OptionNotFoundException::new);
+
+            return new QuestionAnswerModelDTO(id, selectedOptionId);
+        }).toList();
+
+        return new AnswerTestRequestDTO(answerQuestions);
     }
 }
