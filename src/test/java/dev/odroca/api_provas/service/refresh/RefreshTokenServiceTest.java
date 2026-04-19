7a4bda154e07bf4c50bb4ca9a3047d9699134fd7
Davi Rocha <davirochatz@gmail.com>
2026-04-19 12:00:53 -0300

    feat: Adicionado teste de erro quando o refresh token não é enviado

--- src/test/java/dev/odroca/api_provas/service/refresh/RefreshTokenServiceTest.java
+++ src/test/java/dev/odroca/api_provas/service/refresh/RefreshTokenServiceTest.java
@@ -2,12 +2,14 @@ package dev.odroca.api_provas.service.refresh;
 
 import dev.odroca.api_provas.entity.RefreshTokenEntity;
 import dev.odroca.api_provas.entity.UserEntity;
+import dev.odroca.api_provas.exception.UnauthorizedException;
 import dev.odroca.api_provas.repository.RefreshTokenRepository;
 import dev.odroca.api_provas.service.RefreshTokenService;
 import dev.odroca.api_provas.service.utils.RefreshTokenFactory;
 import dev.odroca.api_provas.service.utils.UserFactory;
 import dev.odroca.api_provas.utils.CookieUtil;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,14 +21,12 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 
-import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class RefreshTokenServiceTest {
@@ -43,7 +43,7 @@ public class RefreshTokenServiceTest {
 
     @Test
     @DisplayName("Deve retornar sucesso ao gerar um novo par tokens")
-    void refreshSuccess() {
+    void refreshSuccessTest() {
         UserEntity user = UserFactory.buildUserEntity();
         MockHttpServletRequest request = new MockHttpServletRequest();
         MockHttpServletResponse response = new MockHttpServletResponse();
@@ -61,6 +61,29 @@ public class RefreshTokenServiceTest {
         when(refreshRepository.findByRefreshToken(UUID.fromString(refreshToken.getValue()))).thenReturn(Optional.of(refreshTokenEntity));
 
         refreshService.refresh(request, response);
+
+        verify(cookie, times(2)).addCookie(any(HttpServletResponse.class), any(String.class), any(String.class), any(int.class), any(Boolean.class));
+    }
+
+    @Test
+    @DisplayName("Deve retornar UnauthorizedException quando cookies for nulo")
+    void refreshUnauthorizedExceptionNoCookiesTest() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        assertThrows(UnauthorizedException.class,() -> refreshService.refresh(request, response));
+    }
+
+    @Test
+    @DisplayName("Deve retornar UnauthorizedException quando refresh token não for enviado")
+    void refreshUnauthorizedExceptionWithoutRefreshTokenTest() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        Cookie randomToken = new Cookie("randomToken", "1c465b2a-f088-48a6-a20f-cb54b3d4f571");
+        request.setCookies(randomToken);
+
+        assertThrows(UnauthorizedException.class, () -> refreshService.refresh(request, response));
     }
 
 }
